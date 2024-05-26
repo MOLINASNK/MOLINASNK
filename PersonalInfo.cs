166d67dfc70a6db0ccbeaca059d752325a4780df
MOLINASNK <miguelangelmolina126@gmail.com>
2024-05-26 19:30:24 +0000

    Initial commit

---
+++ PersonalInfo.cs
@@ -0,0 +1,19 @@
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+    
+    // Defining Personal Information
+    string name = "Robert Hall";
+    string birthdate = "2/27/1947";
+    string work = "000-444-0101";
+    string cell = "000-444-0189";
+    
+    // Printing Personal Information
+    Console.WriteLine (name);
+    Console.WriteLine (birthdate);
+    Console.WriteLine ("Work: " + work);
+    Console.WriteLine ("Cell: " + cell);
+  }
+}
+  
